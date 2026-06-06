a31bb03ae0ad83c79a95dbd9163147e8441a5421
André Mauro <andremauro87@gmail.com>
2026-06-05 21:29:13 -0300

    feat: criação da tabela usuarios

--- bd.sql
+++ bd.sql
@@ -6,3 +6,14 @@ nome varchar(80) not null,
 telefone varchar(20) default null,
 email varchar(80) default null
 );
+
+create table usuarios (
+    id int unsigned auto_increment primary key,
+    nome varchar(80) not null,
+    email varchar(80) not null,
+    senha varchar(255) not null,
+    ativo boolean default true,
+    data_de_criacao datetime default current_timestamp,
+    data_de_atualizacao datetime default current_timestamp on update current_timestamp
+);
+
